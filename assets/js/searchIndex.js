e023d24c2fcc60391f7b8a37b8a85f3bda575e48
Jeremie Desautels <desautelsj@hotmail.com>
2017-04-15 22:48:34 +0000

    AppVeyor Publish: e673118952d18cee1275ef95be628e36f525deeb Merge branch 'release/0.3.0'

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -34,8 +34,8 @@ var searchModule = function() {
 
     a({
         id:0,
-        title:"CakeMailSettings",
-        content:"CakeMailSettings",
+        title:"CakeMailResult",
+        content:"CakeMailResult",
         description:'',
         tags:''
     });
@@ -50,8 +50,8 @@ var searchModule = function() {
 
     a({
         id:2,
-        title:"CakeMailResult",
-        content:"CakeMailResult",
+        title:"CakeMailSettings",
+        content:"CakeMailSettings",
         description:'',
         tags:''
     });
@@ -65,8 +65,8 @@ var searchModule = function() {
     });
 
     y({
-        url:'/Cake.CakeMail/Cake.CakeMail/api/Cake.CakeMail.Email/CakeMailSettings',
-        title:"CakeMailSettings",
+        url:'/Cake.CakeMail/Cake.CakeMail/api/Cake.CakeMail/CakeMailResult',
+        title:"CakeMailResult",
         description:""
     });
 
@@ -77,8 +77,8 @@ var searchModule = function() {
     });
 
     y({
-        url:'/Cake.CakeMail/Cake.CakeMail/api/Cake.CakeMail/CakeMailResult',
-        title:"CakeMailResult",
+        url:'/Cake.CakeMail/Cake.CakeMail/api/Cake.CakeMail.Email/CakeMailSettings',
+        title:"CakeMailSettings",
         description:""
     });
 
